831dd4d3171ce0807909acb7242482b77433db32
George Anderson <yun_man_ger_docs@mail.ru>
2011-10-02 13:05:10 +0600

    fix

--- gen/kz/edu/sdu/jg/R.java
+++ gen/kz/edu/sdu/jg/R.java
@@ -58,8 +58,9 @@ public final class R {
     public static final class layout {
         public static final int categories=0x7f030000;
         public static final int companies=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int splash=0x7f030003;
+        public static final int details=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int splash=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
